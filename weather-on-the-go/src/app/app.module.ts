653e23d48707cdc07d7c71e8af8697ad0f85d647
Shefali-Sharma <shefali92@gmail.com>
2018-09-26 22:47:36 -0400

    Adding Place Component and Modules
    Adding Place Component and Modules

--- weather-on-the-go/src/app/app.module.ts
+++ weather-on-the-go/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlaceCreateComponent } from './places/place-create/place-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceCreateComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ weather-on-the-go/src/app/places/place-create/place-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-create',
+  templateUrl: './place-create.component.html',
+  styleUrls: ['./place-create.component.css']
+})
+export class PlaceCreateComponent {
+
+  tempFunc() {
+    console.log('Here');
+  }
+}
