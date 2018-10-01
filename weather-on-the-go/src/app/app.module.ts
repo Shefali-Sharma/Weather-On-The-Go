281b6e861e94589e729637c9b63bfd2791b81ebc
Shefali-Sharma <shefali92@gmail.com>
2018-10-01 16:23:48 -0400

    Adding Header
    Adding Header

--- weather-on-the-go/src/app/app.module.ts
+++ weather-on-the-go/src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
+import {
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule} from '@angular/material';
 import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
 
 import { AppComponent } from './app.component';
 import { PlaceCreateComponent } from './places/place-create/place-create.component';
 import { MapCreateComponent } from './places/map-create/map-create.component';
-import { AgmDirectionModule } from 'agm-direction';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlaceCreateComponent,
-    MapCreateComponent
+    MapCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +29,7 @@ import { AgmDirectionModule } from 'agm-direction';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB09T0-SCd3IMwYWmxOiqknt-7iw2m0wbY'
     }),
