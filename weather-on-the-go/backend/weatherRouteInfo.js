83fc30ee4308c5c03ee88e4829d4b80f99219a10
Shefali-Sharma <shefali92@gmail.com>
2018-10-02 19:12:41 -0400

    Update weatherRouteInfo
    Update weatherRouteInfo

--- weather-on-the-go/backend/weatherRouteInfo.js
+++ weather-on-the-go/backend/weatherRouteInfo.js
@@ -1,14 +1,49 @@
 const geocode = require('./geocode/google_dir');
 const weather = require('./weather/weather-promises');
 
+var getSingleWeather = async(lat, lng) => {
+  let res = await weather.getWeatherInfo(lat, lng);
+  var place = {
+    lat: lat,
+    lng: lng,
+    temp: res
+  };
+  return place;
+};
 
-geocode.getWayPoints('Buffalo, NY', 'Chicago').then((res) => {
-  console.log(res.wayPoints);
-  weather.getWeatherInfo(res.endLoc.lat, res.endLoc.lng).then((response) => {
-    console.log(response);
-  }, (errorMessage) => {
-      console.log(errorMessage);
-  });
+var getAllWeather = async (wayPointsList) => {
+  let output = [];
+  for (const element of wayPointsList){
+    let response = await weather.getWeatherInfo(element.start.lat, element.start.lng);
+    var place = {
+      lat: element.start.lat,
+      lng: element.start.lng,
+      temp: response
+    };
+    output.push(place);
+  }
+  return output;
+};
+
+
+var getWeatherRouteInfo = async (sourceLoc, destinationLoc) => {
+  var res = await geocode.getWayPoints(sourceLoc, destinationLoc);
+  var outS = await getSingleWeather(res.startLoc.lat, res.startLoc.lng);
+  var outE = await getSingleWeather(res.endLoc.lat, res.endLoc.lng);
+  var weather = await getAllWeather(res.wayPoints);
+
+  var finalRes = {
+    start: outS,
+    end: outE,
+    wayPoints: weather
+  }
+  // console.log(weather);
+  return finalRes;
+};
+
+
+getWeatherRouteInfo('Buffalo, NY', 'Chicago').then((response) => {
+  console.log(response);
 }, (errorMessage) => {
   console.log(errorMessage);
 });
